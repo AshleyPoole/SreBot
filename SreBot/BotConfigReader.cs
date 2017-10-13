34e4e0069deefbe522e5a388e90c94dd4e77c340
Ashley Poole <git@ashleypoole.co.uk>
2017-10-13 21:56:29 +0100

    Update general code formatting
    Non-functional commit

--- SreBot/BotConfigReader.cs
+++ SreBot/BotConfigReader.cs
@@ -3,24 +3,27 @@ using Noobot.Core.Configuration;
 
 namespace SreBot
 {
-    public class BotConfigReader : IConfigReader
-    {
-        private readonly IConfigurationSection _configurationSection;
-        private const string SlackApiConfigValue = "slack:apiToken";
+	public class BotConfigReader : IConfigReader
+	{
+		private readonly IConfigurationSection configurationSection;
+		private const string SlackApiConfigValue = "slack:apiToken";
 
-        public BotConfigReader(IConfigurationSection configSection)
-        {
-            _configurationSection = configSection;
-        }
+		public BotConfigReader(IConfigurationSection configSection)
+		{
+			this.configurationSection = configSection;
+		}
 
-        public T GetConfigEntry<T>(string entryName)
-        {
-            return _configurationSection.GetValue<T>(entryName);
-        }
+		public T GetConfigEntry<T>(string entryName)
+		{
+			return this.configurationSection.GetValue<T>(entryName);
+		}
 
-        public string SlackApiKey => GetConfigEntry<string>(SlackApiConfigValue);
-        public bool HelpEnabled { get; set; } = true;
-        public bool StatsEnabled { get; set; } = true;
-        public bool AboutEnabled { get; set; } = false;
-    }
+		public string SlackApiKey => this.GetConfigEntry<string>(SlackApiConfigValue);
+
+		public bool HelpEnabled { get; } = true;
+
+		public bool StatsEnabled { get; } = true;
+
+		public bool AboutEnabled { get; } = false;
+	}
 }
--- SreBot/Startup.cs
+++ SreBot/Startup.cs
@@ -9,42 +9,42 @@ using Microsoft.Extensions.Logging;
 
 namespace SreBot
 {
-    public class Startup
-    {
-        public static IConfigurationRoot Configuration { get; set; }
+	public class Startup
+	{
+		public static IConfigurationRoot Configuration { get; set; }
 
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
 
-            Configuration = builder.Build();
+			Configuration = builder.Build();
 
-            var logConfiguration = new LogConfiguration();
-            Configuration.GetSection("LogConfiguration").Bind(logConfiguration);
-            LogManager.Configure(logConfiguration);
-        }
+			var logConfiguration = new LogConfiguration();
+			Configuration.GetSection("LogConfiguration").Bind(logConfiguration);
+			LogManager.Configure(logConfiguration);
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {            
-        }
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            var noobHost = new BotHost(new BotConfigReader(Configuration.GetSection("Bot")));
-            applicationLifetime.ApplicationStarted.Register(() => noobHost.Start(LogManager.GetLogger<BotHost>()));
-            applicationLifetime.ApplicationStopping.Register(noobHost.Stop);
-        }
-    }
+			var noobHost = new BotHost(new BotConfigReader(Configuration.GetSection("Bot")));
+			applicationLifetime.ApplicationStarted.Register(() => noobHost.Start(LogManager.GetLogger<BotHost>()));
+			applicationLifetime.ApplicationStopping.Register(noobHost.Stop);
+		}
+	}
 }
