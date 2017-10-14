34f9028bf8ac5d3bb05c8e401f310e4f53a7b651
Ashley Poole <git@ashleypoole.co.uk>
2017-10-14 11:26:21 +0100

    Add packages for DNS, NewRelic and LoadBalancer.Org
    Also clean up tabs vs spaces

--- SreBot/BotComponentRegistration.cs
+++ SreBot/BotComponentRegistration.cs
@@ -1,25 +1,35 @@
 ﻿using Noobot.Core.Configuration;
+using Noobot.Modules.Dns;
+using Noobot.Modules.LoadBalancerDotOrg;
+using Noobot.Modules.NewRelic;
 using Noobot.Toolbox.Middleware;
 using Noobot.Toolbox.Plugins;
 
 namespace SreBot
 {
-    public class BotComponentRegistration : ConfigurationBase
-    {
-        public BotComponentRegistration()
-        {
-            this.RegisterCustomComponents();
-            this.RegisterNoobotToolboxComponents();
-        }
+	public class BotComponentRegistration : ConfigurationBase
+	{
+		public BotComponentRegistration()
+		{
+			this.RegisterCustomComponents();
+			this.RegisterNoobotToolboxComponents();
+		}
 
-        private void RegisterCustomComponents()
-        {
-        }
+		private void RegisterCustomComponents()
+		{
+			UseMiddleware<LboMiddleware>();
+			UsePlugin<LboPlugin>();
 
-        private void RegisterNoobotToolboxComponents()
-        {
-            UseMiddleware<ScheduleMiddleware>();
-            UsePlugin<SchedulePlugin>();
-        }
-    }
+			UseMiddleware<NewRelicMiddleware>();
+			UsePlugin<NewRelicPlugin>();
+
+			UseMiddleware<DnsMiddleware>();
+		}
+
+		private void RegisterNoobotToolboxComponents()
+		{
+			UseMiddleware<ScheduleMiddleware>();
+			UsePlugin<SchedulePlugin>();
+		}
+	}
 }
