6bc3f234af83d84a9eec2aa46d0f5460f0f247ef
Ashley Poole <git@ashleypoole.co.uk>
2018-11-19 18:34:09 +0000

    Update Cloudflare and Incident management version

---
+++ src/SreBot.Web/Controllers/NewRelicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using CBot.Modules.NewRelic;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SreBot.Web.Controllers
+{
+	[Route("[controller]")]
+	public class NewRelicController : Controller
+	{
+		private readonly IManageNewRelic newRelicService;
+
+		public NewRelicController(IManageNewRelic newRelicService)
+		{
+			this.newRelicService = newRelicService;
+		}
+
+		[Route("")]
+		public async Task<IActionResult> Index()
+		{
+			var response = await this.newRelicService.GetUnhealthyApplications();
+			return this.View(response.Applications);
+		}
+	}
+}
